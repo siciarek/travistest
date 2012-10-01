9401d90e78d8ebd55637363e9dd15c42941b8197
siciarek <siciarek@gmail.com>
2012-10-01 23:48:26 +0200

    Sample error fix

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set("display_errors","On");
 
-$libdir = dirname(__FILE__) . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR  . "lib";
+$libdir = dirname(__FILE__) . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR  . "lib";
 set_include_path(get_include_path() . PATH_SEPARATOR . $libdir);
 
 require_once "PHPUnit/Autoload.php";
